8e90ce93976cd868d482ae2b6e9fcadb730f8236
carlos <carlospddm@gmail.com>
2020-01-15 09:49:52 +0000

    change

---
+++ u5/p2/bot-demo.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'telegram/bot'
+
+# token:
+# 1. Write your TOKEN value into "token" variable or
+# 2. Create a local file "hiden.token" with your TOKEN value inside
+token = `cat hide.token`.strip
+bot_username = '@cnebher_bot'
+
+puts "[INFO] Running bot #{$0}..."
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    puts " => #{message.text}"
+    if message.text == "/hello"
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello Word!")
+    elsif message.text == "/byebye"
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye bye!")
+    end
+  end
+end
